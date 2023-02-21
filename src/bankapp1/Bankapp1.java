d9a4d2a4efcc2be8d967d27377db1ca1cfae25c2
taranjeetsingh <taranjeetsingh@localhost>
2023-02-21 13:46:43 -0500

    i can i will jdjks

--- src/bankapp1/Bankapp1.java
+++ src/bankapp1/Bankapp1.java
@@ -15,7 +15,7 @@ public class Bankapp1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("Taran");
     }
     
 }
